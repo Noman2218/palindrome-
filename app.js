55291d23b404a365300665e203d750c6288863fd
M.Noman <hayatgil2218@gmail.com>
2024-02-09 17:12:01 +0500

    palindromes'

---
+++ app.js
@@ -0,0 +1,19 @@
+var user = prompt("Enter a Word");
+if(user ==="madam"){
+    alert("This Word is palindrome")
+}
+else if(user === "level"){
+    alert("This Word is palindrome")
+}
+
+else if(user === "refer"){
+    alert("This Word is palindrome")
+}
+
+else if(user === "civic"){
+    alert("This Word is palindrome")
+}
+
+else{
+    alert("This Word is Not palindrome")
+}
